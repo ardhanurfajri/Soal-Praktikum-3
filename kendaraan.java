faea5f54c25ab0104dd3452c4a61c4c945df0ac3
ardhanurfajri <ardha_nur_29rpl@student.smktelkom-mlg.sch.id>
2021-02-16 20:27:37 +0700

    Soal Praktikum 3

---
+++ kendaraan.java
@@ -0,0 +1,24 @@
+  /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Ardha Nur Fajri
+ */
+public class kendaraan {
+    static void spek(){
+        String jenis="Mobil";
+        System.out.println("Jenis kendaraan: "+jenis);
+        int cc=500;
+        System.out.println("Kecepatan kendaraan: "+cc);
+        String brand="BMW";
+        System.out.println("Brand kendaraan: "+brand);
+        int roda=4;
+        System.out.println("Jumlah roda: "+roda);
+        char tipe='A';
+        System.out.println("Tipe kendaraan: "+tipe);
+    }
+}
